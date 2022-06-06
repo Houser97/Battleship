90e8a4bfbcf3ec11b3f9588101908179e250fe9f
Houser97 <arturo.riverar97@gmail.com>
2022-06-06 14:17:47 -0500

    Create gameboard factory function
    At the moment only the first board is created, which contains a method
    capable of creating random positions for the 10 ships.
    
    This method passes in the desired length of the ships, where the first
    3 ships have a length of 3, the next 3 a length of 2, and the last 4 a
    length of 1.
    
    At the outset a ship is created without coordinates and pushed to the
    ships array of the gameboard. Subsequently, with the help of do while
    the random coordinates are created according to the desired ship's
    length. Then, if that location does not contain an already occupied
    coordinate, then it is returned and assigned to the last ship in the
    ships array.

--- src/testScript.js
+++ src/testScript.js
@@ -1,15 +1,82 @@
-// eslint-disable-next-line func-names
-const Ship = function (length) {
-  this.currentHealth = length;
-  const hit = (coordenate) => {
-    this.currentHealth -= 1;
-    return `hit grid${coordenate}`;
+const shipFactory = (coordinates, length) => {
+  let currentHealth = length;
+  const hitArray = [];
+  const hit = (coordinate) => {
+    if (hitArray.includes(coordinate)) {
+      return `grid${coordinate} already hit`;
+    }
+    if (coordinates.includes(coordinate)) {
+      currentHealth -= 1;
+      hitArray.push(coordinate);
+      return `hit grid${coordinate}`;
+    }
+    return "no hit";
   };
   const isSunk = () => {
-    if (this.currentHealth === 0) return "sunk";
+    if (currentHealth === 0) return "sunk";
     return "no sunk";
   };
-  return { hit, isSunk };
+  return { hit, isSunk, coordinates };
 };
 
-module.exports = { Ship };
+const gameboardPlayer1 = (() => {
+  const boardsize = 10;
+  const numberOfShips = 10;
+  const ships = [];
+
+  function generateShip(lengthShip) {
+    const direction = Math.floor(Math.random() * 2);
+    let col;
+    let row;
+    if (direction === 1) {
+      row = Math.floor(Math.random() * boardsize);
+      col = Math.floor(Math.random() * (boardsize - (lengthShip + 1)));
+    } else {
+      row = Math.floor(Math.random() * (boardsize - (lengthShip + 1)));
+      col = Math.floor(Math.random() * boardsize);
+    }
+
+    const newShipLocations = [];
+    for (let i = 0; i < lengthShip; i += 1) {
+      if (direction === 1) {
+        newShipLocations.push(parseInt(`${row}${col + i}`, 10));
+      } else {
+        newShipLocations.push(parseInt(`${row + i}${col}`, 10));
+      }
+    }
+    return newShipLocations;
+  }
+
+  function collision(locations, currentNumberOfShips) {
+    for (let i = 0; i < currentNumberOfShips + 1; i += 1) {
+      const ship = ships[i];
+      for (let j = 0; j < locations.length; j += 1) {
+        if (ship.coordinates.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  const generateShipLocations = () => {
+    let locations;
+    for (let i = 0; i < numberOfShips; i += 1) {
+      let lengthShip;
+      if (i < 3) lengthShip = 3;
+      if (i >= 3 && i < 7) lengthShip = 2;
+      if (i >= 7 && i <= 9) lengthShip = 1;
+      // eslint-disable-next-line prefer-const
+      let ship = shipFactory([], lengthShip);
+      ships.push(ship);
+      do {
+        locations = generateShip(lengthShip);
+      } while (collision(locations, i));
+      ships[i].coordinates = locations;
+    }
+  };
+
+  return { generateShipLocations, ships };
+})();
+
+module.exports = { shipFactory, gameboardPlayer1 };
--- src/testScript.test.js
+++ src/testScript.test.js
@@ -1,9 +1,17 @@
 const index = require("./testScript");
 
 describe("Scope for ship 1", () => {
-  const ship1 = new index.Ship(2);
-  test("check if ship was hit", () => {
-    expect(ship1.hit(13)).toBe("hit grid13");
+  const ship1 = index.shipFactory([2, 3, 4], 3);
+  test("check that ship was hit", () => {
+    expect(ship1.hit(4)).toBe("hit grid4");
+  });
+
+  test("check that coordenate was already hit", () => {
+    expect(ship1.hit(4)).toBe("grid4 already hit");
+  });
+
+  test("check that ship was not hit", () => {
+    expect(ship1.hit(10)).toBe("no hit");
   });
 
   test("check if ship is no sunk", () => {
@@ -12,9 +20,30 @@ describe("Scope for ship 1", () => {
 });
 
 describe("Scope for ship 2", () => {
-  const ship2 = new index.Ship(1);
-  ship2.hit(1);
+  const ship2 = index.shipFactory([10], 1);
+  test("check that ship was hit", () => {
+    expect(ship2.hit(10)).toBe("hit grid10");
+  });
   test("check if ship is sunk", () => {
     expect(ship2.isSunk()).toBe("sunk");
+  });
+});
+
+describe("Scope for player 1 gameboard", () => {
+  test("ship array must have length of 10", () => {
+    index.gameboardPlayer1.generateShipLocations();
+    expect(index.gameboardPlayer1.ships.length).toBe(10);
+  });
+  test("First ship array's element must have length of 3", () => {
+    index.gameboardPlayer1.generateShipLocations();
+    expect(index.gameboardPlayer1.ships[0].coordinates.length).toBe(3);
+  });
+  test("Middle ship array's element must have length of 2", () => {
+    index.gameboardPlayer1.generateShipLocations();
+    expect(index.gameboardPlayer1.ships[4].coordinates.length).toBe(2);
+  });
+  test("Middle ship array's element must have length of 1", () => {
+    index.gameboardPlayer1.generateShipLocations();
+    expect(index.gameboardPlayer1.ships[9].coordinates.length).toBe(1);
   });
 });
